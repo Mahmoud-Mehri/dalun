4f624f1ad69f94d1d0835b87afb3224a938f3736
Mark M <mark.m@pimflare.com>
2023-11-17 11:25:53 +0000

    Initial version of Processor unit added

--- src/processor/processor.go
+++ src/processor/processor.go
@@ -1,3 +1,38 @@
 package processor
 
-var COMMANDS = []string{"use", "add", "delete", "get", ""}
+import (
+	"dalun/models"
+	"time"
+)
+
+// var COMMANDS = []string{"use", "add", "delete", "get", ""}
+
+type Processor struct {
+	DelayTicker *time.Ticker
+	QueueArray  []string
+	Queues      map[string]*models.Queue
+}
+
+func CheckDelays(q *models.Queue) {
+	if len(q.Delayed) > 0 {
+
+	}
+}
+
+func StartProcessor() (*Processor, error) {
+	p := Processor{}
+	// Start Delay Checking Process
+	p.DelayTicker = time.NewTicker(1 * time.Second)
+	go func() {
+		for _ = range p.DelayTicker.C {
+			for _, value := range p.QueueArray {
+				q, found := p.Queues[value]
+				if found {
+					go CheckDelays(q)
+				}
+			}
+		}
+	}()
+
+	return &p, nil
+}
