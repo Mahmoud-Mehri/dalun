194fa0a20f90d134bf34bb3e362d37268dd4d315
Mark M <mark.m@pimflare.com>
2023-11-17 11:22:43 +0000

    Mutexes and constructor function added to Queue object

--- src/models/queue.go
+++ src/models/queue.go
@@ -1,11 +1,52 @@
 package models
 
+import (
+	"sync"
+	"time"
+)
+
 type Queue struct {
-	Id          int
-	Name        string
-	Delayed     map[int]Job
-	Ready       map[int]Job
-	Reserved    map[int]Job
-	Ignored     map[int]Job
-	Subscribers map[int]Subscriber
+	Id   int
+	Name string
+
+	DelayLocker sync.Mutex
+	Delayed     map[int]*Job
+
+	ReadyLocker sync.Mutex
+	Ready       map[int]*Job
+
+	ReservedLocker sync.Mutex
+	Reserved       map[int]*Job
+
+	IgnoredLocker sync.Mutex
+	Ignored       map[int]*Job
+
+	SubscriberLocker sync.Mutex
+	Subscribers      map[int]*Subscriber
+}
+
+func NewQueue(name string) (*Queue, error) {
+	queue := Queue{}
+	queue.Name = name
+
+	return &queue, nil
+}
+
+func (q *Queue) AddNewJob(data []byte, delay int, expire time.Time) (*Job, error) {
+	job, err := NewJob(data, delay, expire)
+	if err != nil {
+		return nil, err
+	}
+
+	if job.Delay > 0 {
+		q.DelayLocker.Lock()
+		q.Delayed[job.Id] = job
+		q.DelayLocker.Unlock()
+	} else {
+		q.ReadyLocker.Lock()
+		q.Ready[job.Id] = job
+		q.ReadyLocker.Unlock()
+	}
+
+	return job, nil
 }
