34657b984057e89ebcc1accb7fa1a38a291517bf
Mark M <mark.m@pimflare.com>
2023-11-17 11:24:26 +0000

    Initial version of AddNewJob function added for Queue model

--- src/models/queue.go
+++ src/models/queue.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"sync"
+	"time"
 )
 
 type Queue struct {
@@ -29,4 +30,23 @@ func NewQueue(name string) (*Queue, error) {
 	queue.Name = name
 
 	return &queue, nil
+}
+
+func (q *Queue) AddNewJob(data []byte, delay int, expire time.Time) (*Job, error) {
+	job, err := NewJob(data, delay, expire)
+	if err != nil {
+		return nil, err
+	}
+
+	if job.Delay > 0 {
+		q.DelayLocker.Lock()
+		q.Delayed[job.Id] = job
+		q.DelayLocker.Unlock()
+	} else {
+		q.ReadyLocker.Lock()
+		q.Ready[job.Id] = job
+		q.ReadyLocker.Unlock()
+	}
+
+	return job, nil
 }
