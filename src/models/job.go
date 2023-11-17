4d3a9835bedf26fe3709f751da1d9c77f9e42063
Mark M <mark.m@pimflare.com>
2023-11-17 11:21:04 +0000

    NewJob function added for creating new Job object

--- src/models/job.go
+++ src/models/job.go
@@ -9,3 +9,12 @@ type Job struct {
 	CreatedAt time.Time
 	ExpireAt  time.Time
 }
+
+func NewJob(data []byte, delay int, expire time.Time) (*Job, error) {
+	job := Job{}
+	copy(job.Data, data)
+	job.Delay = time.Duration(delay * 1000000)
+	job.ExpireAt = expire
+
+	return &job, nil
+}
