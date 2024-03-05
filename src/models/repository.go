48b93b5992ebd9d1b96d5bd537001a99a9990c7f
Mahmoud Mehri <mahmoud.mehri90@gmail.com>
2024-03-05 10:16:59 +0400

    DeleteQueue added to Repository

--- src/models/repository.go
+++ src/models/repository.go
@@ -1,8 +1,15 @@
 package models
 
+import "errors"
+
 type JobRepository struct {
-	QueueArray []string
+	queueArray []string
 	Queues     map[string]*Queue
+}
+
+// Get Queue List
+func (repo *JobRepository) GetQueueNames() []string {
+	return repo.queueArray
 }
 
 // Adding new Queue
@@ -15,11 +22,27 @@ func (repo *JobRepository) AddQueue(qname string) error {
 	println("Queue Created:", qname)
 
 	repo.Queues[qname] = q
-	repo.QueueArray = append(repo.QueueArray, qname)
+	repo.queueArray = append(repo.queueArray, qname)
 
 	println("Queue Added:", qname)
 
 	return nil
+}
+
+func (repo *JobRepository) DeleteQueue(qname string) error {
+	if repo.Queues[qname] != nil {
+		delete(repo.Queues, qname)
+		for i, val := range repo.queueArray {
+			if val == qname {
+				repo.queueArray = append(repo.queueArray[:i], repo.queueArray[i+1:]...)
+				break
+			}
+		}
+
+		return nil
+	}
+
+	return errors.New(ERROR_QUEUE_NOTFOUND_MSG)
 }
 
 // Adding new Job
