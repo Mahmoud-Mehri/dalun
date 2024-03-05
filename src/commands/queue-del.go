eaa05cdc665fe4eec8fbc90577cd5af3e97c09cd
Mahmoud Mehri <mahmoud.mehri90@gmail.com>
2024-03-05 10:18:11 +0400

    Command updated: queue-del

--- src/commands/queue-del.go
+++ src/commands/queue-del.go
@@ -1,8 +1,36 @@
 package commands
 
-import "dalun/models"
+import (
+	"dalun/models"
+	"strings"
+)
 
 func DelQueueCommand(repo *models.JobRepository, cmd string) *models.CommandResult {
+	result := models.CommandResult{}
 
-	return nil
+	commandParts := strings.Split(cmd, " ")
+	if !((len(commandParts) == 2) && (commandParts[0] == "queue-del")) {
+		result.Success = false
+		result.Error = models.CommandError{
+			Code:    models.ERROR_INVALID_FORMAT,
+			Message: models.ERROR_INVALID_FORMAT_MSG,
+		}
+		return &result
+	}
+
+	queueName := commandParts[1]
+
+	err := repo.DeleteQueue(queueName)
+	if err != nil {
+		result.Success = false
+		result.Error = models.CommandError{
+			Code:    models.ERROR_INTERNAL,
+			Message: models.ERROR_INTERNAL_MSG,
+		}
+	} else {
+		result.Success = true
+		result.Data = "QUEUE_DELETED"
+	}
+
+	return &result
 }
