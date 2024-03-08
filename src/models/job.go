8340398a7f9ec7bf2bbda7888df48d0545ab0b19
Mahmoud Mehri <mahmoud.mehri90@gmail.com>
2024-03-08 11:23:49 +0400

    Job Id converted to uint type in Job model

--- src/models/job.go
+++ src/models/job.go
@@ -3,15 +3,17 @@ package models
 import "time"
 
 type Job struct {
-	Id          int
+	Id          uint
 	Data        []byte
 	CreatedAt   time.Time
 	Delay       time.Duration
 	ExpireAfter time.Duration
 }
 
-func NewJob(data []byte, delay int, expireAfter int) (*Job, error) {
-	job := Job{}
+func NewJob(id uint, data []byte, delay int, expireAfter int) (*Job, error) {
+	job := Job{
+		Id: id,
+	}
 	copy(job.Data, data)
 	job.Delay = time.Duration(int(time.Second) * delay)
 	job.ExpireAfter = time.Duration(int(time.Second) * expireAfter)
